2f7c1570c18e8980a4b7c751de254135d4cec2f8
hadar <hadarpinto@gmail.com>
2021-10-23 17:20:51 +0300

    linear reg added

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -31,12 +31,11 @@ float var(float* x, int size){
 float cov(float* x, float* y, int size){
     float Ex = avg(x, size);
     float Ey = avg(y, size);
-    float xy = (*)x * (*)y;
+    float xy = (*x) * (*y);
     float* xy_f = &xy;
     float Exy = avg(xy_f, size);
 
     float cov = Exy - (Ex*Ey);
-
 	return cov;
 }
 
@@ -48,8 +47,19 @@ float pearson(float* x, float* y, int size){
 
 // performs a linear regression and returns the line equation
 Line linear_reg(Point** points, int size){
+    int arrSize = sizeof(points) / sizeof(points[0]);
+    float sumX = 0, sumY = 0, sumPowX = 0, sumXY = 0, a, b;
+    for (int i = 0; i < arrSize; i++) {
+        sumX += *points[i]->x;
+        sumY += *points[i]->y;
+        sumPowX += (*points[i]->x) * (*points[i]->x);
+        sumXY += (*points[i]->x) * (*points[i]->y);
+    }
 
-	return Line(0,0);
+    a = (arrSize*sumXY - sumX*sumY) / (arrSize*sumPowX - sumX*sumX);
+    b = (sumY - a*sumX) / arrSize;
+
+	return Line(a,b);
 }
 
 // returns the deviation between point p and the line equation of the points
