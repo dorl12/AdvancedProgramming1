2f56d5c626758372c18581220802f6ecf37ac188
hadar <hadarpinto@gmail.com>
2021-10-23 18:57:49 +0300

    last update

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -10,11 +10,12 @@
 
 float avg(float* x, int size){
     float sum = 0;
-    for(int i = 1; i <= size;i++){
-        sum += *x;
+    float average;
+    for(int i = 0; i < size;i++){
+        sum += x[i];
     }
-    sum = sum / (float)size;
-	return sum;
+    average = sum / (float)size;
+	return average;
 }
 
 // returns the variance of X and Y
@@ -22,26 +23,25 @@ float var(float* x, int size){
     float sqDiff = 0;
     float mean = avg(x, size);
     for (int i = 0; i < size; i++) {
-        sqDiff += (*x - mean) * (*x - mean);
+        sqDiff += (x[i] - mean) * (x[i] - mean);
     }
-    return sqDiff / n;
+    return sqDiff / (float)size;
 }
 
 // returns the covariance of X and Y
 float cov(float* x, float* y, int size){
-    float Ex = avg(x, size);
-    float Ey = avg(y, size);
-    float xy = (*x) * (*y);
-    float* xy_f = &xy;
-    float Exy = avg(xy_f, size);
-    float cov = Exy - (Ex*Ey);
-	return cov;
+    float cov = 0;
+    for (int i = 0; i < size; i++){
+        cov += (x[i] - avg(x,size)) * (y[i] - avg(y,size));
+    }
+
+	return cov / size;
 }
 
 
 // returns the Pearson correlation coefficient of X and Y
 float pearson(float* x, float* y, int size){
-	float corr = cov(x, y, size) / sqrt(avg(x, size)) * sqrt(avg(y, size));
+	float corr = cov(x, y, size) / ((sqrt(var(x, size))) * (sqrt(var(y, size))));
     return corr;
 }
 
@@ -50,10 +50,10 @@ Line linear_reg(Point** points, int size){
     int arrSize = sizeof(points) / sizeof(points[0]);
     float sumX = 0, sumY = 0, sumPowX = 0, sumXY = 0, a, b;
     for (int i = 0; i < arrSize; i++) {
-        sumX += *points[i]->x;
-        sumY += *points[i]->y;
-        sumPowX += (*points[i]->x) * (*points[i]->x);
-        sumXY += (*points[i]->x) * (*points[i]->y);
+        sumX += points[i]->x;
+        sumY += points[i]->y;
+        sumPowX += (points[i]->x) * (points[i]->x);
+        sumXY += (points[i]->x) * (points[i]->y);
     }
     a = (arrSize*sumXY - sumX*sumY) / (arrSize*sumPowX - sumX*sumX);
     b = (sumY - a*sumX) / arrSize;
@@ -65,7 +65,7 @@ Line linear_reg(Point** points, int size){
 float dev(Point p,Point** points, int size){
     Line line;
     line = linear_reg(points, size);
-    float linearVal = line.a * p.x + b;
+    float linearVal = line.a * p.x + line.b;
     float distance = abs(linearVal - p.y);
 
 	return distance;
@@ -73,7 +73,7 @@ float dev(Point p,Point** points, int size){
 
 // returns the deviation between point p and the line
 float dev(Point p,Line l){
-    float linearVal = l.a * p.x + b;
+    float linearVal = l.a * p.x + l.b;
     float distance = abs(linearVal - p.y);
     return distance;
 }
