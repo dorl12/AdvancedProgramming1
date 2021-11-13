83402a5c5ce60dbb08cb9ba71270cf4ac4a9b8e5
dorl12 <dorl1204@gmail.com>
2021-11-13 17:35:14 +0200

    timeSeries class

--- timeseries.cpp
+++ timeseries.cpp
@@ -1,3 +1,35 @@
+#include <sstream>
 #include "timeseries.h"
 
+TimeSeries::TimeSeries(const char* CSVfileName) {
+    ifstream file(CSVfileName);
+    initializeFeatures(file);
+    initializeDataList(file);
+    file.close();
+}
 
+void TimeSeries::initializeFeatures(ifstream file) {
+    string firstLine = "";
+    getline(file,firstLine);
+    stringstream ss(firstLine);
+    while (ss.good()) {
+        string substr;
+        getline(ss, substr, ',');
+        features.push_back(substr);
+    }
+}
+
+void TimeSeries::initializeDataList(ifstream file) {
+    string line = "";
+    while (getline(file,line)) {
+        stringstream ss(line);
+        for (int i = 0; i < features.size();) {
+            while (ss.good()) {
+                string substr;
+                getline(ss, substr, ',');
+                dataList[i].push_back(substr);
+                i++;
+            }
+        }
+    }
+}
--- timeseries.h
+++ timeseries.h
@@ -2,18 +2,18 @@
 
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
-
+#include "fstream"
+#include "iostream"
+#include "vector"
 using namespace std;
 
 class TimeSeries{
-
+    vector<string> features;
+    vector<vector<string>> dataList;
 public:
-    
-	TimeSeries(const char* CSVfileName){
-	}
-
+    TimeSeries(const char* CSVfileName);
+    void initializeFeatures(ifstream file);
+    void initializeDataList(ifstream file);
 };
-
-
 
 #endif /* TIMESERIES_H_ */
