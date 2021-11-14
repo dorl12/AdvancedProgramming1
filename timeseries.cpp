f0edfbdbe26ed39848148a6dbc8fa067e7e90409
dorl12 <dorl1204@gmail.com>
2021-11-14 17:54:31 +0200

    timeSeries class final version

--- timeseries.cpp
+++ timeseries.cpp
@@ -1,35 +1,37 @@
 #include <sstream>
 #include "timeseries.h"
 
-TimeSeries::TimeSeries(const char* CSVfileName) {
-    ifstream file(CSVfileName);
+TimeSeries::TimeSeries(const char *CSVfileName) {
+    fstream file(CSVfileName);
     initializeFeatures(file);
     initializeDataList(file);
     file.close();
 }
 
-void TimeSeries::initializeFeatures(ifstream file) {
-    string firstLine = "";
-    getline(file,firstLine);
+void TimeSeries::initializeFeatures(fstream &file) {
+    string firstLine;
+    getline(file, firstLine);
     stringstream ss(firstLine);
     while (ss.good()) {
         string substr;
         getline(ss, substr, ',');
-        features.push_back(substr);
+        this->features.push_back(substr);
     }
 }
 
-void TimeSeries::initializeDataList(ifstream file) {
-    string line = "";
-    while (getline(file,line)) {
+void TimeSeries::initializeDataList(fstream &file) {
+    vector<vector<float>> dataListPer(this->features.size(),vector<float>());
+    string line;
+    while (getline(file, line)) {
         stringstream ss(line);
-        for (int i = 0; i < features.size();) {
+        for (int i = 0; i < this->features.size();) {
             while (ss.good()) {
                 string substr;
                 getline(ss, substr, ',');
-                dataList[i].push_back(substr);
+                dataListPer[i].push_back(stof(substr));
                 i++;
             }
         }
     }
+    this->dataList = dataListPer;
 }
--- timeseries.h
+++ timeseries.h
@@ -2,18 +2,18 @@
 
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
-#include "fstream"
-#include "iostream"
-#include "vector"
+#include <fstream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 class TimeSeries{
     vector<string> features;
-    vector<vector<string>> dataList;
+    vector<vector<float>> dataList;
 public:
     TimeSeries(const char* CSVfileName);
-    void initializeFeatures(ifstream file);
-    void initializeDataList(ifstream file);
+    void initializeFeatures(fstream &file);
+    void initializeDataList(fstream &file);
 };
 
 #endif /* TIMESERIES_H_ */
