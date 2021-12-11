4b358c8f8ee7155151fe1f0fac0f4fd952eafb04
dorl12 <83288357+dorl12@users.noreply.github.com>
2021-12-11 12:43:16 +0200

    Update minCircle.h
    final version minCircle.h

--- minCircle.h
+++ minCircle.h
@@ -1,4 +1,9 @@
-
+/*
+ * minCircle.h
+ *
+ * Author: 316460146 Hadar Pinto
+ *         313547085 Dor Levy
+ */
 #ifndef MINCIRCLE_H_
 #define MINCIRCLE_H_
 
@@ -21,8 +26,32 @@ public:
 };
 // --------------------------------------
 
+// returns the minimum circle enclosing the given points
 Circle findMinCircle(Point** points,size_t size);
+// returns the euclidean distance between two points
+double eucDist(const Point& firstPoint, const Point& secondPoint);
+// checks whether the given point lies inside the circle or on the boundaries
+bool isInsideTheCircle(const Circle& circle, const Point& point);
+// returns a point which is the center of a circle defined by three points
+Point calculateCenter(double x1, double y1, double x2, double y2);
+// returns the smallest circle defined by three points
+Circle circleFromThreePoints(const Point& firstPoint, const Point& secondPoint, const Point& thirdPoint);
+// returns the smallest circle defined by two points
+Circle circleFromTwoPoints(const Point& firstPoint, const Point& secondPoint);
+// checks whether a circle encloses the given points
+bool isValidCircle(const Circle& circle, const vector<Point>& vectorOfPoints);
+// returns the minimum circle of up to three points
+Circle trivialMinCircle(vector<Point> vectorOfPoints);
+/*
+    returns the minimum circle using Welzl's algorithm
+    the function gets an array of pointers to points,
+    a vector of points on the boundary of the circle
+    and a number of points in the array which are not
+    yet processed and returns the minimum circle that
+    contains all the points
+ */
+Circle minCircleWelzlAlgo(Point** inputPoints, vector<Point> pointsOnBoundary, int numOfPoints);
 
-// you can add here additional methods
 
 #endif /* MINCIRCLE_H_ */
+
